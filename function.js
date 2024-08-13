4d539c63ad595b4d3dfa2f003c5e86a743adfd69
leonardo szarblewski <leonardolaurindo103@gmail.com>
2024-08-13 12:47:19 -0300

    exercicios-função-objetos

---
+++ function.js
@@ -0,0 +1,5 @@
+function adi (num1, num2, num3){return num1 + num2 - num3}
+
+console.log(adi(5, 5, 3));
+
+/*exemplo de como usar função*/
---
+++ objetos.js
@@ -0,0 +1,16 @@
+const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Software Engineer',
+  };
+  
+  // Criação da função
+  const addProperty = (object, key, value) => {
+    if (typeof object[key] === "undefined"){
+      object[key] = value;
+    }
+  };
+  
+  addProperty(customer, 'altura', '1.85', 'email');
+  
+  console.log(customer);
